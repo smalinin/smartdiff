43cea16a7bdd8c4c920830551952345ccc18f402
Sergey Malinin <sergmalinin@gmail.com>
2026-06-24 23:46:49 +0300

    Fix Nuitka macos

--- smartdiff_nuitka_macos_x86.py
+++ smartdiff_nuitka_macos_x86.py
@@ -1,11 +1,11 @@
 # nuitka-project: --mode=standalone
 # nuitka-project: --enable-plugin=pyside6
-# nuitka-project: --output-dir=dist/nuitka_macos
+# nuitka-project: --output-dir=dist/nuitka_macos_x86
 # nuitka-project: --remove-output
 # nuitka-project: --assume-yes-for-downloads
 # nuitka-project-if: {OS} == "Darwin":
 #    nuitka-project: --macos-create-app-bundle
-#    nuitka-project: --macos-target-arch=arm64
+#    nuitka-project: --macos-target-arch=x86_64
 #    nuitka-project: --output-filename=SmartDiff
 
 from smartdiff.app import main
